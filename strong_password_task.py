a97f05ace5974e62ea259bc72a50ce1dd0961850
ivanvikvik <ivanvikvik@gmail.com>
2022-10-20 21:23:45 +0300

    Update check_password function after refactoring with module string and any()/all() functions

--- strong_password_task.py
+++ strong_password_task.py
@@ -1,6 +1,4 @@
-DIGITS = "0123456789"
-LOWER_CASE = "qwertyuiopasdfghhjklzxcvbnm"
-UPPER_CASE = "QWERTYUIOPASDFGHJKKLZXCVBNMN"
+import string
 
 
 def check_password(password):
@@ -10,53 +8,24 @@ def check_password(password):
         return -1
 
     password = password.strip()
+    digit = string.digits
+    lower = string.ascii_lowercase
+    upper = string.ascii_uppercase
 
     # logic
     if len(password) < 8:
         return "too weak"
 
     # weak
-    is_digit = True
-    for ch in password:
-        if ch not in DIGITS:
-            is_digit = False
-            break
-
-    is_lower = True
-    for ch in password:
-        if ch not in LOWER_CASE:
-            is_lower = False
-            break
-
-    is_upper = True
-    for ch in password:
-        if ch not in UPPER_CASE:
-            is_upper = False
-            break
-
-    if is_upper or is_lower or is_digit:
+    if (all(ch in digit for ch in password)
+            or all(ch in lower for ch in password)
+            or all(ch in upper for ch in password)):
         return "weak"
 
     # very strong
-    is_digit = False
-    for ch in password:
-        if ch in DIGITS:
-            is_digit = True
-            break
-
-    is_lower = False
-    for ch in password:
-        if ch in LOWER_CASE:
-            is_lower = True
-            break
-
-    is_upper = False
-    for ch in password:
-        if ch in UPPER_CASE:
-            is_upper = True
-            break
-
-    if is_lower and is_upper and is_digit:
+    if (any(ch in digit for ch in password)
+            and any(ch in lower for ch in password)
+            and any(ch in upper for ch in password)):
         return "very strong"
 
     return "strong"
--- test_string_module.py
+++ test_string_module.py
@@ -1,11 +1,27 @@
 import string
 
-print(string.octdigits)
-print(string.digits)
-print(string.hexdigits)
-print(string.printable)
-print(string.whitespace)
-print(string.punctuation)
-print(string.ascii_letters)
-print(string.ascii_lowercase)
-print(string.ascii_uppercase)
+ls = [i * 10 for i in range(10)]
+print(ls)
+ls = [i * i for i in range(10)]
+print(ls)
+
+s = "qwertyuiop"
+ls = [i * 3 for i in s]
+print(ls)
+
+s = "cvb"
+STR = "qri"
+ls = [0, 0]
+print(ls)
+print(all(ls))
+print(any(ls))
+
+# print(string.octdigits)
+# print(string.digits)
+# print(string.hexdigits)
+# print(string.printable)
+# print(string.whitespace)
+# print(string.punctuation)
+# print(string.ascii_letters)
+# print(string.ascii_lowercase)
+# print(string.ascii_uppercase)
